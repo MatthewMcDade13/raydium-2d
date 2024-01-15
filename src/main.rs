0827a5af3f7ebe5fe7ff87a2a411bbd3ab4c66c6
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2024-01-14 21:39:21 -0800

    extracting raycaster into run() fn

--- src/main.rs
+++ src/main.rs
@@ -6,89 +6,12 @@ mod render;
 mod shader;
 
 use anyhow::anyhow;
+use raycast::run;
 use raydium::render::QuadRenderer;
 
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
 
 fn main() -> anyhow::Result<()> {
-    // Show logs from wgpu
-
-    let sdl_context = sdl2::init().map_err(|e| anyhow!(e))?;
-    let video_subsystem = sdl_context.video().map_err(|e| anyhow!(e))?;
-    let window = video_subsystem
-        .window("Raw Window Handle Example", 800, 600)
-        .position_centered()
-        .resizable()
-        .vulkan()
-        .build()?;
-    let (width, height) = window.size();
-
-    let mut event_pump = sdl_context.event_pump().map_err(|e| anyhow!(e))?;
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                // Event::Window {
-                //     window_id,
-                //     win_event: WindowEvent::SizeChanged(width, height),
-                //     ..
-                // } if r.has_window(window_id) => {
-                //     r.resize(width as u32, height as u32);
-                // }
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'running;
-                }
-                e => {
-                    dbg!(e);
-                }
-            }
-        }
-
-        // let frame = match surface.get_current_texture() {
-        //     Ok(frame) => frame,
-        //     Err(err) => {
-        //         let reason = match err {
-        //             SurfaceError::Timeout => "Timeout",
-        //             SurfaceError::Outdated => "Outdated",
-        //             SurfaceError::Lost => "Lost",
-        //             SurfaceError::OutOfMemory => "OutOfMemory",
-        //         };
-        //         panic!("Failed to get current surface texture! Reason: {}", reason)
-        //     }
-        // };
-        //
-        // let output = frame
-        //     .texture
-        //     .create_view(&wgpu::TextureViewDescriptor::default());
-        // let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-        //     label: Some("command_encoder"),
-        // });
-        //
-        // {
-        //     let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-        //         color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-        //             view: &output,
-        //             resolve_target: None,
-        //             ops: wgpu::Operations {
-        //                 load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-        //                 store: wgpu::StoreOp::Store,
-        //             },
-        //         })],
-        //         depth_stencil_attachment: None,
-        //         label: None,
-        //         ..Default::default()
-        //     });
-        //     rpass.set_pipeline(&render_pipeline);
-        //     rpass.set_bind_group(0, &bind_group, &[]);
-        //     rpass.draw(0..3, 0..1);
-        // }
-        // queue.submit([encoder.finish()]);
-        // frame.present();
-    }
-
-    Ok(())
+    run()
 }
--- src/raycast.rs
+++ src/raycast.rs
@@ -1,13 +1,91 @@
+use std::ops::{Add, Mul};
+
 use anyhow::anyhow;
+use nalgebra_glm as glm;
 use sdl2::{
+    event::Event,
+    keyboard::Keycode,
     pixels::PixelFormatEnum,
     render::{Canvas, Texture, TextureCreator},
     video::{Window, WindowContext},
 };
 
-use nalgebra_glm as glm;
-
 use crate::gfx;
+
+pub const WORLD_MAP: [[u8; 24]; 24] = [
+    [
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ],
+];
 
 pub struct RaycastRenderer {
     canvas: Canvas<Window>,
@@ -64,4 +142,105 @@ impl RaycastRenderer {
         self.canvas.set_draw_color(color);
         self.canvas.clear();
     }
+
+    fn raycast_screen(&mut self, player: &Player, cam: &Camera) {
+        let w = self.surface_size.x;
+        for x in 0..self.surface_size.x {
+            let camx = (2 * x) as f32 / (w as f32) - 1.0;
+            let ray_dir = cam.plane.mul(camx) + player.dir;
+
+            let map_pos = glm::vec2(player.pos.x as i32, player.pos.y as i32);
+
+            // length of ray from one x or y-side to next x or y-side
+            let delta_dist = {
+                let dx = if ray_dir.x == 0.0 {
+                    std::f32::INFINITY
+                } else {
+                    (1.0 / ray_dir.x).abs()
+                };
+
+                let dy = if ray_dir.y == 0.0 {
+                    std::f32::INFINITY
+                } else {
+                    (1.0 / ray_dir.y).abs()
+                };
+                glm::vec2(dx, dy)
+            };
+
+            let mut side_dist = glm::vec2(0., 0.);
+            let mut perp_wall_dist = 0.;
+
+            // what diretion to step in x or y-direction (either +1 or -1)
+            let mut step = glm::vec2(0, 0);
+
+            let mut hit = 0;  // was there a wall hit?
+            let mut side = 0; // was a North-South or East-West wall hit
+            
+
+            if ray_dir.x < 0. {
+                step.x = -1;
+                side_dist.x = (player.pos.x -)
+            }
+
+            // if ray_dir.x == 0.0 || ray_dir.y == 0.0 {
+            //     glm::vec2(std::f32::INFINITY, std::f32::INFINITY)
+            // } else {
+            //     ray_dir.abs().normalize()
+            // };
+        }
+    }
+}
+
+struct Player {
+    pos: glm::Vec2,
+    dir: glm::Vec2,
+}
+
+struct Camera {
+    plane: glm::Vec2,
+}
+
+pub fn run() -> anyhow::Result<()> {
+    // Show logs from wgpu
+
+    let sdl_context = sdl2::init().map_err(|e| anyhow!(e))?;
+    let video_subsystem = sdl_context.video().map_err(|e| anyhow!(e))?;
+    let window = video_subsystem
+        .window("Raw Window Handle Example", 800, 600)
+        .position_centered()
+        .resizable()
+        .vulkan()
+        .build()?;
+    let (width, height) = window.size();
+
+    let mut r = RaycastRenderer::new(window)?;
+
+    let mut player = Player {
+        pos: glm::vec2(22., 12.),
+        dir: glm::vec2(-1., 0.),
+    };
+    let mut cam = Camera {
+        plane: glm::vec2(0., 0.66),
+    };
+
+    let mut event_pump = sdl_context.event_pump().map_err(|e| anyhow!(e))?;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                e => {
+                    dbg!(e);
+                }
+            }
+        }
+
+        r.raycast_screen(&player, &cam);
+    }
+    Ok(())
 }
